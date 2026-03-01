cae25389af407142ebade37bcf27d466c5766e61
kristina-dt <christi.dt23@gmail.com>
2026-03-01 23:15:25 +0300

    creating a new branch Visualization

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,25 +1,15 @@
 #include "Map.h"
+#include "GameEngine.h"
 #include <iostream>
-Map::Map() {
-    map = {
-        "------------------------------",
-        "|                            |",
-        "|                            |",
-        "|                            |",
-        "|                            |",
-        "|                            |",
-        "|                            |",
-        "|                            |",
-        "|                            |",
-        "|                            |",
-        "|                            |",
-        "|                            |",
-        "------------------------------",
-        };
-}
 
-void Map::draw() const{
-    for (const auto& row : map) {
-        std::cout << row << '\n';
+void Map::WholeMap(const std::vector<std::string> &map, const Player& player) {
+    for (int i = 0; i < GameEngine::WIDTH; ++i) {
+        for (int j = 0; j < GameEngine::HEIGHT; ++j) {
+            if (i==(player.getX()) && j==(player.getY())) {
+                std::cout<<'[^]';
+            }
+            std::cout << map[i][j];
+        }
     }
+    std::cout<<'\n';
 }
