137d5fccb31d6e7e4a2c4becbff04595ee2ef08d
vitchk <vorobevavioletta971@gmail.com>
2026-03-06 00:07:24 +0300

    Add class definition to class Appliance

--- include/Appliance.h
+++ include/Appliance.h
@@ -1,8 +1,21 @@
-//
-// Created by Воробьева Виолетта on 05.03.2026.
-//
+#pragma once
+#include <string>
+#include <memory>
 
-#ifndef SPACE_S_APPLIANCE_H
-#define SPACE_S_APPLIANCE_H
+class Appliance {
+public:
+    virtual ~Appliance() = default;
 
-#endif //SPACE_S_APPLIANCE_H
+    virtual std::string getType() const = 0;
+    virtual bool produce(Player& player) = 0;
+    virtual void upgrade() = 0;
+    virtual int getLevel() const { return level; }
+    virtual int getUpgradeCost() const = 0;
+
+    int getProductionRate() const { return baseProductionRate * (1 + level * 0.5); }
+
+protected:
+    int level = 1;
+    int baseProductionRate = 10;
+    std::string name;
+};
