550224b075d792b1bac234a1cdb118abb143843a
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-06 01:27:19 +0300

    add a part of code for Wallet

--- include/Wallet.h
+++ include/Wallet.h
@@ -1,11 +1,19 @@
 
 
-#ifndef SPACE_S_WALLET_H
-#define SPACE_S_WALLET_H
-
+#pragma once
+#include <ostream>
+#include "NotEnoughMoney.h"
 
 class Wallet {
+private:
+    int balance;
+public:
+    Wallet (int initialAmount) noexcept;
+    Wallet (const Wallet&) = default;
+    Wallet& operator=(const Wallet) = delete;
+    Wallet(Wallet&& other) noexcept;
+
+
 };
 
 
-#endif
