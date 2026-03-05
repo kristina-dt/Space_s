9a960cb2acb328c8ed18d0fccb985be7bf43b8c0
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-06 00:29:36 +0300

    add class NotEnoughMoney

---
+++ include/NotEnoughMoney.h
@@ -0,0 +1,13 @@
+//
+// Created by Arina on 06.03.2026.
+//
+
+#ifndef MYPROJECT_NOTENOUGHMONEY_H
+#define MYPROJECT_NOTENOUGHMONEY_H
+
+
+class NotEnoughMoney {
+};
+
+
+#endif //MYPROJECT_NOTENOUGHMONEY_H
