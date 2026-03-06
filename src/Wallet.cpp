a6af9384fd0023c2c27c41978d4c0d471b8c47c7
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-06 12:59:21 +0300

    add initialAmount, balance for other and void withdraw in Wallet.cpp

--- src/Wallet.cpp
+++ src/Wallet.cpp
@@ -1,2 +1,22 @@
 #include "Wallet.h"
 #include <string>
+
+Wallet::Wallet(int initialAmount) noexcept : balance(initialAmount) {
+    if (initialAmount<0) {
+        balance = 0;
+    }
+}
+Wallet::Wallet(Wallet &&other) noexcept : balance(other.balance) {
+    other.balance = 0;
+}
+void Wallet::withdraw(int amount) {
+    if (amount > balance) {
+        throw NotEnoughMoney( "Not enough money. Need:" + std::to_string(amount) +
+            ", available: " + std::to_string(balance));
+    }
+    balance -= amount;
+}
+
+
+
+
