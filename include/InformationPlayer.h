6fa8926492b7e67036ce431495999314e71a2e9a
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-06 13:16:34 +0300

    add classes InformationPlayer

---
+++ include/InformationPlayer.h
@@ -0,0 +1,13 @@
+//
+// Created by Arina on 06.03.2026.
+//
+
+#ifndef SPACE_S_INFORMATIONPLAYER_H
+#define SPACE_S_INFORMATIONPLAYER_H
+
+
+class InformationPlayer {
+};
+
+
+#endif //SPACE_S_INFORMATIONPLAYER_H
---
+++ src/InformationPlayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Arina on 06.03.2026.
+//
+
+#include "../InformationPlayer.h"
