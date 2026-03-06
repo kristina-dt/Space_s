008070ba6a41c32f66e2f174955c5d219ada0cd1
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-07 01:12:33 +0300

    add func: getX, getY, getLevel, getWal, Setposition, upgradeWorkshop, buyResourse, addResourse, getName, getSex, sellTakeMoney in InformationPlayer.h

--- include/InformationPlayer.h
+++ include/InformationPlayer.h
@@ -23,6 +23,22 @@ private:
     std::map<Resource::Type, int> buyPrice;
     std::map<Resource::Type, int> sellPrice;
 public:
+    InformationPlayer (const std::string namePlayer, const std::string s, int a, float py, float px, int startMoney);
+    std::string getName()const;
+    std::string getSex()const;
+    constexpr float getX()const;
+    constexpr float getY()const;
+    int getLevel()const;
+    Wallet& getWal();
+    void SetPosition(float x, float y);
+    void upgradeWorkShop(int cost);
+    bool addResource(Resource::Type type, int amount);
+    bool buyResource(Resource::Type type, int amount);
+    bool sell(Resource::Type type, int amount);
+    bool sellTakeMoney(Resource::Type type, int amount);
+
+
+
 
 
 
