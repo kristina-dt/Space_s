9654eba4b4d9b62e1c6c34685488cb5248a48f9f
shudjixx <yugreta2008@gmail.com>
2026-03-08 12:53:46 +0300

    Add abstract Order base class for Orders module Defines virtual interface for all order types: - typeName() - describe() - priority() - rewardMoney() - clone()

--- include/Order.h
+++ include/Order.h
@@ -13,6 +13,14 @@ enum class ResourceType : std::uint8_t { Fuel, Food, Drinks, Details, Decoration
 std::string to_string(ResourceType t);
 
 class Order {
+public:
+    virtual ~Order() = default;
+    virtual std::string typeName() const = 0;
+    virtual std::string describe() const = 0;
+    virtual int priority() const noexcept = 0;
+    virtual int rewardMoney() const noexcept = 0;
+
+    virtual std::unique_ptr<Order> clone() const = 0;
 };
 
 
