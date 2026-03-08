c07d6ae8990ffb7a4ad1b4f2befa14c38987f82d
shudjixx <yugreta2008@gmail.com>
2026-03-08 13:32:12 +0300

    feat: add ResourceOrder class derived from Order

--- include/Order.h
+++ include/Order.h
@@ -37,6 +37,21 @@ public:
 
     virtual std::unique_ptr<Order> clone() const = 0;
 };
+class ResourceOrder final : public Order {
+private:
+    ResourceType resource_;
+    int amount_;
+    int baseReward_;
+public:
+    ResourceOrder(ResourceType resource, int amount, int baseReward);
+    std::string typeName() const override;
+    std::string describe() const override;
+    int priority() const noexcept override;
+    int rewardMoney() const noexcept override;
+    ResourceType resourceType() const noexcept override;
+    int amount() const noexcept override;
+    std::unique_ptr<Order> clone() const override;
+};
 
 
 #endif //SPACE_S_ORDER_H
