9d13e2a6617423d40ef3cf5d720e3b05b2ebec69
vitchk <vorobevavioletta971@gmail.com>
2026-03-08 12:05:27 +0300

    Correct class Appliance

--- include/Appliance.h
+++ include/Appliance.h
@@ -1,21 +1,21 @@
+// Appliance.h
 #pragma once
-#include <string>
-#include <memory>
+
 
 class Appliance {
 public:
     virtual ~Appliance() = default;
-
     virtual std::string getType() const = 0;
     virtual bool produce(Player& player) = 0;
-    virtual void upgrade() = 0;
-    virtual int getLevel() const { return level; }
     virtual int getUpgradeCost() const = 0;
 
-    int getProductionRate() const { return baseProductionRate * (1 + level * 0.5); }
+    virtual void upgrade();
+    int getLevel() const { return level_; }
+    std::string getName() const { return name_; }
 
 protected:
-    int level = 1;
-    int baseProductionRate = 10;
-    std::string name;
+    Appliance() = default;x
+
+    int level_ = 1;
+    std::string name_;
 };
--- src/Appliance.cpp
+++ src/Appliance.cpp
@@ -1,1 +1,7 @@
 #include "Appliance.h"
+#include <iostream>
+
+void Appliance::upgrade() {
+    level_++;
+    std::cout << name_ << " upgraded to level " << level_ << "!\n";
+}
