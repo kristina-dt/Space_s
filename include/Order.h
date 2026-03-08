8d715a2d34206d5dee484df7aa076bc293543e54
shudjixx <yugreta2008@gmail.com>
2026-03-08 12:15:13 +0300

    add class Order

---
+++ include/Order.h
@@ -0,0 +1,13 @@
+//
+// Created by Greta Yugay on 08.03.2026.
+//
+
+#ifndef SPACE_S_ORDER_H
+#define SPACE_S_ORDER_H
+
+
+class Order {
+};
+
+
+#endif //SPACE_S_ORDER_H
---
+++ src/Order.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Greta Yugay on 08.03.2026.
+//
+
+#include "Order.h"
