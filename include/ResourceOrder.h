8aa24644427b222569c249cad97924fd0138eda7
shudjixx <yugreta2008@gmail.com>
2026-03-08 16:20:51 +0300

    Add ResourceOrder derived from Order

--- include/ResourceOrder.h
+++ include/ResourceOrder.h
@@ -1,6 +1,23 @@
 #pragma once
 
-class ResourceOrder {
+#include "Order.h""
+
+class ResourceOrder final : public Order {
+private:
+    Resource::Type resource_;
+    int amount_;
+    int baseReward_;
+public:
+    ResourceOrder(Resource::Type resource, int amount, int baseReward);
+    std::string typeName() const override;
+    std::string describe() const override;
+    int priority() const noexcept override;
+    int rewardMoney() const noexcept override;
+
+    Resource::Type resourceType() const noexcept override;
+    int amount() const noexcept override;
+
+    std::unique_ptr<Order> clone() const override;
 };
 
 
