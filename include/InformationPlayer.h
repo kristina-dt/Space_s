91edb4d2ef3330a6354f1477a199179da1dad642
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-10 15:04:59 +0300

    delete errores in infoplayer

--- include/InformationPlayer.h
+++ include/InformationPlayer.h
@@ -16,15 +16,17 @@ private:
     std::string sex;
     int age;
     std::vector<Resource> inventory;
-    static constexpr float posX = 2.2, posY=2.2;
+    float posX , posY;
     Wallet wallet;
     std::map<Resource::Type, int> sellPrice;
 public:
+    static constexpr float startX = 2.0f;
+    static constexpr float startY = 2.2f;
     InformationPlayer (const std::string namePlayer, const std::string s, int a, float py, float px, int startMoney);
     std::string getName()const;
     std::string getSex()const;
-    constexpr float getX()const{return posX;};
-    constexpr float getY()const{return posY;};
+    float getX()const{return posX;};
+    float getY()const{return posY;};
     Wallet& getWal();
     void SetPosition(float x, float y);
     bool sell(Resource::Type type, int amount);
