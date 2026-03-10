3a4b6e3e53df07184375f2cfa02d2b355f610bad
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-10 12:21:59 +0300

    add func for main (infoPlayer)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include "include/GameEngine.h"
-
+#include "include/InformationPlayer.h"
+#include <iostream>
+InformationPlayer createPlayer(){
+    std::string name, sex;
+    int age;
+    std::cout << "Creation of Character\n";
+    std::cout << "==================\n\n";
+    std::cout << "Write person's name: ";
+    std::getline(std::cin, name);
+    std::cout << "Write sex: ";
+    std::getline(std::cin, sex);
+    std::cout << "Write age: ";
+    std::cin >> age;
+    int startMoney = 1000;
+    float startX = 5.0f, startY = 5.0f;
+    std::cout << "Initial balance: " << startMoney << " credits\n";
+    return InformationPlayer(name, sex, age, startY, startX, startMoney);
+}
 int main() {
     GameEngine m;
     m.drawOnce();
+
+
     return 0;
 }
